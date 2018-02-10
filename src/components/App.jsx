428f1f62a31608eb9eb4003923bfba1d5b357eca
Cheryl Frankenfield <cherylfrankenfield@gmail.com>
2018-02-09 16:48:08 -0800

    Add Error404 component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Nav from './Nav';
 import Footer from './Footer';
 import Home from './Home';
 import Admin from './Admin';
+import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Home}/>
         <Route path='/admin' component={Admin}/>
+        <Route component={Error404} />
       </Switch>
       <Footer />
     </div>
